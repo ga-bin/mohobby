31ead690e9e057f3c67e2d88067c99edcd99cdf1
bhy <bhy9987@gmail.com>
2022-09-29 13:54:19 +0900

    moim sns vo추가

---
+++ mohobby/src/main/java/com/yedam/mohobby/service/sns/SnsBookmarkCatgVO.java
@@ -0,0 +1,10 @@
+package com.yedam.mohobby.service.sns;
+
+import lombok.Data;
+
+@Data
+public class SnsBookmarkCatgVO {
+	private int catgId;
+	private String memberId;
+	private String catgName;
+}
