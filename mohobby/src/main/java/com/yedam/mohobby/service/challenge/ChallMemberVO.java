eed8185a5f33b702a7315d38c2c92ce8ddc85f65
SAENDIN <SAENDIN@DESKTOP-2QAFC2K>
2022-09-30 16:22:59 +0900

    ChallengeMemberVO/Sns

--- mohobby/src/main/java/com/yedam/mohobby/service/challenge/ChallMemberVO.java
+++ mohobby/src/main/java/com/yedam/mohobby/service/challenge/ChallMemberVO.java
@@ -7,15 +7,17 @@ import lombok.Data;
 @Data
 //챌린저스 가입 유저
 public class ChallMemberVO {
-    private int ptpId; // 참가ID
-    private int challId; // 챌린지ID
-    private String memberId; // 회원아이디
-    private int ptpFee; // 등록한 상금
-    private int certCnt; // 회원 인증 횟수
-    private String bankCode; //은행코드
-    private String account; // 계좌번호
-    private Date receiveDate; // 금액입금일자
-    private int clearYn; //성공여부
-    private int receivePrice; // 입금금액
-	private int profileYn; //0: 공개 1:비공개
+  private int ptpId; // 참가ID
+  private int challId; // 챌린지ID
+  private String memberId; // 회원아이디
+  private int ptpFee; // 등록한 상금
+  private int certCnt; // 인증횟수_회원
+  private String bankCode; //은행코드
+  private String account; // 계좌번호
+  private Date receiveDate; // 상금입금일자
+  private int clearYn; //성공여부
+  private int receivePrice; // 수령금액
+  private int profileYn; // 프로필공개여부
+  private int challRate; // 달성율
+  private int accountHolder; // 예금주
 }
