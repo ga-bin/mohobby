8c7a82684a9d4c02d9810a9376d9e71efc90154a
User <User@DESKTOP-FMNN920>
2022-10-04 06:04:25 +0900

    rrrrr

--- mohobby/mohobby/src/views/admin/AdminChallengeView.vue
+++ mohobby/mohobby/src/views/admin/AdminChallengeView.vue
@@ -1,7 +1,55 @@
 <template>
     <main>
         <AdminSidebar></AdminSidebar>
-        <h3>승인 대기 중인 챌린지</h3>
+         <v-row align="center" justify="space-around">
+            <v-btn depressed>챌린지 등록하러 가기</v-btn>
+         </v-row>
+         <br>
+        <h3>챌린지 목록</h3>
+         <v-col class="py-2" v-for="(category, index) in categorys" :key="index">
+            <v-btn>{{ category }}</v-btn>
+        </v-col>
+        <br>
+        <br>
+            <v-row v-for="item in challlist" :key="item" class="challItem" >
+              <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+          {{ item.title }}
+        </v-list-item-title>
+        <v-list-item-subtitle>{{ item.content }}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+      >
+        {{ item.category }}
+      </v-btn>
+    <v-btn
+      depressed
+      color="primary"
+    >정산대기</v-btn>
+    </v-card-actions>
+  </v-card>
+            </v-row>
+        <br>
+        <br>
+        <h3>승인 대기 중인 인증샷</h3>
          <v-col class="py-2" v-for="(category, index) in categorys" :key="index">
             <v-btn>{{ category }}</v-btn>
         </v-col>
@@ -22,9 +70,10 @@
 </template>
 <script>
 import AdminSidebar from "../../components/admin/AdminSidebar.vue";
+import challengeInfo from "../../components/admin/challengeInfo.vue";
 export default {
     name: '',
-    components: { AdminSidebar },
+    components: { AdminSidebar, challengeInfo },
     data() {
       return {
             categorys: ['공예', '운동', '음악', '미술', '기타' ],
@@ -85,6 +134,13 @@ export default {
             certYn: '승인됨'
           },
         ],
+        challlist : [
+          {title : "하루에 운동 10분하기", content : "어떤 운동이든 하루에 10분하고 인증샷 남기기", category : "운동"},
+          {title : "하루에 운동 10분하기", content : "어떤 운동이든 하루에 10분하고 인증샷 남기기", category : "운동"},
+          {title : "하루에 운동 10분하기", content : "어떤 운동이든 하루에 10분하고 인증샷 남기기", category : "운동"},
+          {title : "하루에 운동 10분하기", content : "어떤 운동이든 하루에 10분하고 인증샷 남기기", category : "운동"},
+          {title : "하루에 운동 10분하기", content : "어떤 운동이든 하루에 10분하고 인증샷 남기기", category : "운동"}
+        ]
       }
     },
     beforeCreate() {},
@@ -102,5 +158,14 @@ export default {
 .py-2 {
     display: inline-block;
     width: 80px;
+}
+
+.challItem {
+  display : inline-block;
+  margin : 10px;
+}
+
+.challItem > div {
+  width: 450px;
 }
 </style>
