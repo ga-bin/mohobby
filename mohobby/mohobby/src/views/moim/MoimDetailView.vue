40d41ac134fa3baeb493bb4e6988a1bc2074bb3e
jjoem <guswjd0151@naver.com>
2022-10-06 01:10:18 +0900

    현졍

--- mohobby/mohobby/src/views/moim/MoimDetailView.vue
+++ mohobby/mohobby/src/views/moim/MoimDetailView.vue
@@ -5,23 +5,38 @@
   <v-card-actions>
     <v-spacer></v-spacer>
     <v-btn color="mr-3" @click="click()">Sidebar</v-btn>
-    <v-btn class="mr-3"><v-icon>mdi-account-plus</v-icon></v-btn>
-    <v-btn class="mr-3"><v-icon>mdi-lead-pencil</v-icon></v-btn>
+    <v-card-actions v-if="(moim===0 && moimRight===1) || (moim===1 && moimRight===1)">
+      <v-btn class="mr-3"><v-icon>mdi-account-plus</v-icon></v-btn>
+      <v-btn class="mr-3"><v-icon>mdi-lead-pencil</v-icon></v-btn>
+    </v-card-actions>
+    <v-card-actions v-if="(moim===0 && moimRight===0) || (moim===1 && moimRight===0)">
+      <v-btn disabled class="mr-3"><v-icon>mdi-account-plus</v-icon></v-btn>
+      <v-btn disabled class="mr-3"><v-icon>mdi-lead-pencil</v-icon></v-btn>
+    </v-card-actions>
   </v-card-actions>
-</div>
+  </div>
+
   <!-- 컴포넌트 -->
   <MoimSidebar v-if="fclick === true"></MoimSidebar>
-  <MoimBoard></MoimBoard>
+
+  <!-- 권한에 따라 컴포넌트 다르게 보여주기 -->
+  <BoardNoAuthority v-if="moim===0 && moimRight===0"></BoardNoAuthority>
+  <MoimBoard v-else></MoimBoard>
   </div>
 </template>
 <script>
-  import MoimSidebar from "../../components/moim/moimDetail/MoimSidebar.vue";
-  import MoimBoard from "../../components/moim/moimDetail/MoimBoard.vue";
+  import MoimSidebar from "@/components/moim/moimDetail/MoimSidebar.vue";
+  import MoimBoard from "@/components/moim/moimDetail/board/MoimBoard.vue";
+  import BoardNoAuthority from "@/components/moim/moimDetail/board/BoardNoAuthority.vue"
 
   export default {
-  components: { MoimSidebar, MoimBoard },
+  components: { MoimSidebar, MoimBoard, BoardNoAuthority },
   data: () => ({
+
     fclick: true,
+    // 비공개 : 0, 공개 : 1
+    moim: 1,
+    moimRight : 0
     }),
   setup() {},
   created() {},
