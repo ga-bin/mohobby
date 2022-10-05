39b387fdb924773597e2b0651da53dabbc174ea5
Hsame <party3658@naver.com>
2022-10-05 17:52:26 +0900

    LHD1005

---
+++ mohobby/mohobby/src/views/moim/MoimScheduleView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MoimSidebar></MoimSidebar>
+    <MoimSchedule></MoimSchedule>
+  </div>
+</template>
+<script>
+  import MoimSidebar from "../../components/moim/moimDetail/MoimSidebar.vue";
+  import MoimSchedule from "../../components/moim/moimDetail/MoimSchedule.vue";
+  export default {
+  components: { MoimSidebar, MoimSchedule},
+  data: () => ({
+    }),
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+<style>
+
+</style>
---
+++ mohobby/mohobby/src/views/moim/MoimVoteView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MoimSidebar></MoimSidebar>
+    <MoimPhoto></MoimPhoto>
+  </div>
+</template>
+<script>
+  import MoimSidebar from "../../components/moim/moimDetail/MoimSidebar.vue";
+  import MoimPhoto from "../../components/moim/moimDetail/MoimPhoto.vue";
+  export default {
+  components: { MoimSidebar, MoimPhoto},
+  data: () => ({
+    }),
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+<style>
+
+</style>
