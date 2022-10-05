1aee4fbe68d3d12b374f47012cf84dc67a1fff95
Hsame <party3658@naver.com>
2022-10-05 17:51:58 +0900

    1005LHD

--- mohobby/src/main/java/com/yedam/mohobby/service/admin/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.yedam.mohobby.service.admin;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface MemberService {
-	public MemberVO getMember(String memberId);
-}
