5ca0f4c78f813fec60bca8ee317861ddfdd6c1db
dmskk <eek4416@gmail.com>
2022-10-06 17:51:05 +0900

    iamport

--- mohobby/mohobby/src/main.js
+++ mohobby/mohobby/src/main.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import router from './router';
+import axios from 'axios';
+import IMP from 'vue-iamport';
+import $ from 'jquery';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
