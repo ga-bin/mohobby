ec1cba61d1a37a175810a19716a7c56f76a6c090
jjoem <guswjd0151@naver.com>
2022-10-07 17:52:15 +0900

    현정

--- mohobby/mohobby/src/components/moim/moimDetail/schedule/MoimSchedule.vue
+++ mohobby/mohobby/src/components/moim/moimDetail/schedule/MoimSchedule.vue
@@ -1,11 +1,44 @@
-<template>
-  <div>
-    <span>ScheduleTest</span>
-  </div>
+<!-- <template>
+  <v-app>
+    <div
+      class="ma-4"
+      style="height: 100%"
+    >
+      <v-calendar
+        :events="events"
+        now="2022-04-17"
+      ></v-calendar>
+    </div>
+  </v-app>
 </template>
+
 <script>
-
-</script>
-<style>
-
-</style>
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      events: [
+        {
+          name: 'Event 1',
+          start: '2022-04-01',
+          timed: false,
+          color: 'green',
+        },
+        {
+          name: 'Event 2',
+          start: '2022-04-05',
+          end: '2022-04-07',
+          color: 'yellow darken-3',
+        },
+        {
+          name: 'Event 3',
+          start: '2022-04-09T08:00:00',
+          end: '2022-04-09T10:00:00',
+          timed: true,
+          color: 'red',
+        },
+      ],
+    };
+  },
+};
+</script> -->
