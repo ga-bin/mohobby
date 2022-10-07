d56e9f4bf1a84804e4257d1033f1adfa0082f4a3
ga-bin <rlarkqlskb@naver.com>
2022-10-07 09:31:41 +0900

    dddd

--- mohobby/mohobby/src/main.js
+++ mohobby/mohobby/src/main.js
@@ -1,17 +1,18 @@
-import Vue from 'vue';
-import App from './App.vue';
-import vuetify from './plugins/vuetify';
-import router from './router';
-import axios from 'axios';
-import $ from 'jquery';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import axios from "axios";
+import $ from "jquery";
 
 Vue.config.productionTip = false;
 
 Vue.prototype.axios = axios;
 Vue.prototype.$ = $;
+axios.defaults.baseURL = "http://localhost:8888/java";
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
