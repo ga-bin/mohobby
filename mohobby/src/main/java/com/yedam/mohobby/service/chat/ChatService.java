59211fe99dafd1e332a7027ccd2479837277db00
codingtrainghjh <kwonho88@gmail.com>
2022-10-11 19:26:46 +0900

    bhy 1011

--- mohobby/src/main/java/com/yedam/mohobby/service/chat/ChatService.java
+++ mohobby/src/main/java/com/yedam/mohobby/service/chat/ChatService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 @Service
 public interface ChatService {
 public List<RoomVO>getChatRoom(String memberId);
+public List<RoomVO>getChatMoimRoom(String memberId);
 public List<ChatVO>getChat(String roomNo);
 public ChatUserVO getOtherUser(ChatUserVO chatUserVO);
 public int insertMessage(MessageVO message);
+public int CreateRoom(CreateRoomVO cr);
 }
---
+++ mohobby/src/main/java/com/yedam/mohobby/service/chat/CreateRoomVO.java
@@ -0,0 +1,9 @@
+package com.yedam.mohobby.service.chat;
+
+import lombok.Data;
+
+@Data
+public class CreateRoomVO {
+	private String myId;
+	private String targetId;
+}
