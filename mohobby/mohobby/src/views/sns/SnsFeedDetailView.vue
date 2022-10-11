99067ecb7f828da197475e23ac8fc9ba89cb60ba
codingtrainghjh <kwonho88@gmail.com>
2022-10-11 19:26:57 +0900

    bhy 1011

--- mohobby/mohobby/src/views/sns/SnsFeedDetailView.vue
+++ mohobby/mohobby/src/views/sns/SnsFeedDetailView.vue
@@ -1,40 +1,33 @@
 <template>
-  <div id="container"> 
+  <div id="container">
     <SnsSidebar></SnsSidebar>
     <h1>피드디테일</h1>
     <v-container>
-      <v-card class="mx-auto"
-              max-width="1000">
+      <v-card class="mx-auto" max-width="1000">
         <v-row>
           <v-col col="12">
 
-        <!-- 프로필 -->
-        <div>
-          <div class="flex">
-            <v-avatar 
-              class="ml-10 my-10 mr-4" 
-              color="grey darken-1" 
-              size="64"
-              >
-                <v-img
-                  aspect-ratio="30"
-                  :src="require(`@/assets/image/sns/${items.profileImg}`)"
-                />
-              </v-avatar>
-            <div class="user text-overline">{{items.memberId}}<br>{{  this.$moment(items.writeDate).format('YYYY.MM.DD') }}</div>
-          </div>
-        </div>
-          <div id="mdi-dots-vertical">
-            <v-btn icon>
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-          </div>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col col="12">
-          <div id="image_box">
-          <!-- <div class="d-flex flex-column justify-space-between align-center">
+            <!-- 프로필 -->
+            <div>
+              <div class="flex">
+                <v-avatar class="ml-10 my-10 mr-4" color="grey darken-1" size="64">
+                  <v-img aspect-ratio="30" :src="require(`@/assets/image/sns/${items.profileImg}`)" />
+                </v-avatar>
+                <div class="user text-overline">{{items.memberId}}<br>{{
+                this.$moment(items.writeDate).format('YYYY.MM.DD') }}</div>
+              </div>
+            </div>
+            <div id="mdi-dots-vertical">
+              <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col col="12">
+            <div id="image_box">
+              <!-- <div class="d-flex flex-column justify-space-between align-center">
             <v-slider
               v-model="width"
               class="align-self-stretch"
@@ -42,26 +35,23 @@
               max="600"
               step="1"
             ></v-slider> -->
-            <!-- <v-img v-for="(item, i) in imagelist" :key="i" :src="require(`../../../back/uploads/${item}`)"
+              <!-- <v-img v-for="(item, i) in imagelist" :key="i" :src="require(`../../../back/uploads/${item}`)"
        contain height="150px" width="200px" style="border: 2px solid black; margin-left:100px;"/> -->
-       <v-carousel ref="myCarousel" hide-delimiters :touchless="ture">
-            <v-carousel-item
-              :aspect-ratio="4 / 3"
-              :width="width"
-              :src="require(`@/assets/image/sns/${items.thumbnail}`)"
-            ></v-carousel-item>
-          </v-carousel>
-          <!-- </div> -->
-        </div>
-      </v-col>
-    </v-row>
-      <v-col col="12">
-        <div>{{ items.content }}</div>
-      </v-col>
-      <v-col col="12">
-        <div>{{ items.hashtag }}</div>
-      </v-col>
-      <div style="display=flex;">
+              <v-carousel ref="myCarousel" hide-delimiters :touchless="ture">
+                <v-carousel-item :aspect-ratio="4 / 3" :width="width"
+                  :src="require(`@/assets/image/sns/${items.thumbnail}`)"></v-carousel-item>
+              </v-carousel>
+              <!-- </div> -->
+            </div>
+          </v-col>
+        </v-row>
+        <v-col col="12">
+          <div>{{ items.content }}</div>
+        </v-col>
+        <v-col col="12">
+          <div>{{ items.hashtag }}</div>
+        </v-col>
+        <div style="display=flex;">
           <div id="like_box">
             <v-btn icon id="full_heart" text @click="likeBtn">
               <v-icon color="red lighten-2">mdi-heart</v-icon>
@@ -76,13 +66,15 @@
             {{ items.cmts }}
           </div>
           <div>
-            <v-icon @click="send">mdi-send</v-icon>
+            <div v-if="this.$store.state.id !== items.memberId">
+              <v-icon @click="send">mdi-send</v-icon>
+          </div>
           </div>
         </div>
       </v-card>
       <v-col col="12">
-          <Comment></Comment>
-        </v-col>
+        <Comment></Comment>
+      </v-col>
     </v-container>
   </div>
 </template>
@@ -98,14 +90,12 @@ export default {
     likeStatus: 0, //좋아요 없음
     width: 800,
     roomId: 0,
-    items:[],
+    items: [],
   }),
-  setup() {},
+  setup() { },
   created() {
-      console.log(this.$route.query.id);
-      console.log(this.$store.state.id);
-      this.showDetail();
-    },
+    this.showDetail();
+  },
   mounted() {
     // For touch devices
     // this.$refs.myCarousel.$el.addEventListener("touchmove", (e) => {
@@ -132,94 +122,106 @@ export default {
     //   this.move = [];
     // });
   },
-  unmounted() {},
+  unmounted() { },
   methods: {
     //Detail조회
-    showDetail(){
+    showDetail() {
       let postId = this.$route.query.id;
-        this.axios('/sns/user/feed_detail', {
-            params : {
-              postId : postId
-            }
-        }).then(res => {
-          this.items = res.data;
-          console.log(this.items);
-          console.log(this.items.cmts);
-        }).catch(err =>{
-            console.log(err);
-        });
+      this.axios('/sns/user/feed_detail', {
+        params: {
+          postId: postId
+        }
+      }).then(res => {
+        this.items = res.data;
+        console.log(this.items);
+        console.log(this.items.cmts);
+      }).catch(err => {
+        console.log(err);
+      });
     },
+
     send() {
-     this.$router.push({name:"chat",params:{roomId:this.roomId}})
+      let vm = this;
+      this.axios.post('http://localhost:8088/java/CreateRoom/', {
+        myId: vm.$store.state.id,
+        tarId: vm.items.memberId
+      })
+        .then(function (res) {
+          console.log(res);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+      //this.$router.push({name:"chat",params:{roomId:this.roomId}})
     },
     likeBtn() {
       let memberId = this.$store.state.id;
       let postId = this.$route.query.id;
       //좋아요
       // let target = event.target.getElementById;
-      if(!memberId) {
+      if (!memberId) {
         alert('로그인이 필요합니다!');
-      } else{
+      } else {
         if (this.likeStatus == 0) {
           this.showFullHeart();
           console.log(this.showFullHeart);
           this.likeStatus = 1; // DB로 업데이트
-        
+
           this.axios
             .post("/sns/like", {
               memberId: memberId,
               targetId: postId,
             })
             .then(function (response) {
-              console.log("좋아요: "+response);
+              console.log("좋아요: " + response);
             })
             .catch(function (error) {
-              console.log("좋아요실패: "+error);
+              console.log("좋아요실패: " + error);
             });
-            this.axios
+          this.axios
             .put("/sns/like", {
-              params : {
+              params: {
                 targetId: postId,
                 postId: postId,
               }
             })
             .then(function (response) {
-              console.log("좋아요수 업댓성공: "+response);
+              console.log("좋아요수 업댓성공: " + response);
             })
             .catch(function (error) {
-              console.log("좋아요수 업댓실패: "+error);
+              console.log("좋아요수 업댓실패: " + error);
             });
-      } else { //좋아요 취소
-        this.showEmptyHeart();
-        console.log(this.showEmptyHeart);
-        this.likeStatus = 0;
+        } else { //좋아요 취소
+          this.showEmptyHeart();
+          console.log(this.showEmptyHeart);
+          this.likeStatus = 0;
 
-        this.axios
-          .delete("/sns/like", {
-            params : {
-              memberId: memberId,
-              targetId: postId,
-            }
-          })
-          .then(function (response) {
-            console.log("좋아요삭제: "+response);
-          })
-          .catch(function (error) {
-            console.log("좋아요삭제 실패: "+error);
-          });
           this.axios
-          .put("/sns/like", {
-            params : {
-              targetId: postId,
-              postId: postId,
-            }
-          })
-          .then(function (response) {
-            console.log("좋아요수 업댓성공: "+response);
-          })
-          .catch(function (error) {
-            console.log("좋아요수 업댓실패: "+error);
-          });
+            .delete("/sns/like", {
+              params: {
+                memberId: memberId,
+                targetId: postId,
+              }
+            })
+            .then(function (response) {
+              console.log("좋아요삭제: " + response);
+            })
+            .catch(function (error) {
+              console.log("좋아요삭제 실패: " + error);
+            });
+          this.axios
+            .put("/sns/like", {
+              params: {
+                targetId: postId,
+                postId: postId,
+              }
+            })
+            .then(function (response) {
+              console.log("좋아요수 업댓성공: " + response);
+            })
+            .catch(function (error) {
+              console.log("좋아요수 업댓실패: " + error);
+            });
         }
       }
     },
@@ -273,9 +275,11 @@ export default {
   width: 550px;
   height: 500px;
 }
-.box{
-  display:inline-block
+
+.box {
+  display: inline-block
 }
+
 #like_box {
   width: 550;
   margin: 0 auto;
