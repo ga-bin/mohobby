d4a9d796fb369f96b3da5b1cf1aa0bfc020703ff
dmskk <eek4416@gmail.com>
2022-10-12 05:19:23 +0900

    class

--- mohobby/src/main/java/com/yedam/mohobby/mapper/classes/ClassMapper.java
+++ mohobby/src/main/java/com/yedam/mohobby/mapper/classes/ClassMapper.java
@@ -1,5 +1,7 @@
 package com.yedam.mohobby.mapper.classes;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.yedam.mohobby.service.classes.ClassesVO;
@@ -8,4 +10,7 @@ import com.yedam.mohobby.service.classes.ClassesVO;
 public interface ClassMapper {
 	//강의개설
 	public void insertClass(ClassesVO classes);
+	
+	//전체조회
+	public List<ClassesVO> listAll();
 }
