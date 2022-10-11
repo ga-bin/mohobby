83393ec9d454769b9ebd6f9ba35360e80a1c10c3
rlarkqlskb@naver.com <rlarkqlskb@naver.com>
2022-10-11 21:26:18 +0900

    커

--- mohobby/src/main/java/com/yedam/mohobby/mapper/moim/MoimMapper.java
+++ mohobby/src/main/java/com/yedam/mohobby/mapper/moim/MoimMapper.java
@@ -1,28 +1,28 @@
-package com.yedam.mohobby.mapper.moim;
-
-import java.util.List;
-
-import com.yedam.mohobby.service.moim.MoimVO;
-
-public interface MoimMapper {
-	//전체조회
-	public List<MoimVO> moimAllSelect();
-	
-	//소모임 등록
-	public void moimInsert (MoimVO moimVO);
-
-	//소모임 모집 멤버
-	public List<MoimVO> moimrecruitMember();
-	
-	//인기소모임 조회
-	public List<MoimVO> moimPopularSelect();
-	
-	//소모임명 조회
-	public List<MoimVO> moimNameSelect();
-	
-	//소모임 카테고리 조회
-	public List<MoimVO> moimCatgSelect();
-	
-	//소모임 종합 검색
-	public List<MoimVO> moimAllSearch();
-}
+//package com.yedam.mohobby.mapper.moim;
+//
+//import java.util.List;
+//
+//import com.yedam.mohobby.service.moim.MoimVO;
+//
+//public interface MoimMapper {
+//	//전체조회
+//	public List<MoimVO> moimAllSelect();
+//	
+//	//소모임 등록
+//	public void moimInsert (MoimVO moimVO);
+//
+//	//소모임 모집 멤버
+//	public List<MoimVO> moimrecruitMember();
+//	
+//	//인기소모임 조회
+//	public List<MoimVO> moimPopularSelect();
+//	
+//	//소모임명 조회
+//	public List<MoimVO> moimNameSelect();
+//	
+//	//소모임 카테고리 조회
+//	public List<MoimVO> moimCatgSelect();
+//	
+//	//소모임 종합 검색
+//	public List<MoimVO> moimAllSearch();
+//}
