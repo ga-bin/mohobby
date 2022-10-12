0525bf32c3f409c612c42cbd460db2fd5b1457b4
ga-bin <rlarkqlskb@naver.com>
2022-10-13 06:04:10 +0900

    1013마이페이지 구현중

---
+++ mohobby/mohobby/src/views/user/EditUserView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <UserSidebar></UserSidebar>
+    <h1>회원정보 수정하기</h1>
+  </div>
+</template>
+<script>
+import UserSidebar from "../../components/user/UserSidebar.vue";
+
+export default {
+  name: "",
+  components: { UserSidebar },
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+
+<style></style>
