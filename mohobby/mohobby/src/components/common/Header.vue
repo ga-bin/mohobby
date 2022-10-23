28dae79ae606199193533f1854c227d0209bcf5d
bhy <bhy@DESKTOP-6BAVQTB>
2022-10-23 14:59:04 +0900

    bhy 1023 259

--- mohobby/mohobby/src/components/common/Header.vue
+++ mohobby/mohobby/src/components/common/Header.vue
@@ -1,22 +1,21 @@
 <template>
   <v-app-bar app color="white" elevate-on-scroll elevation="4">
-    <v-btn @click="test()" icon>
-      <v-icon>mdi-arrow-left-box</v-icon>
-    </v-btn>
     <v-toolbar-title @click="$router.push('/').catch(() => {})" style="cursor: pointer">Mohobby</v-toolbar-title>
-    <p>{{ this.$store.state.id }}</p>
+    <!-- <p>{{ this.$store.state.id }}</p> -->
+    <v-spacer/>
+    <p>{{ this.$store.state.id }}님, 오늘 모하비?</p>
     <v-spacer />
     <v-btn text class="ml-2" to="/snsmain">sns</v-btn>
     <v-btn text class="ml-2" to="/class/list/all">강의</v-btn>
     <v-btn text class="ml-2" to="/moimmain">소모임</v-btn>
-    <v-btn text class="ml-2" to="/challengemain">챌린지</v-btn>
     <v-spacer />
     <v-col lg="4" cols="12">
       <v-form class="mt-5">
-        <v-text-field rounded outlined dense placeholder="Search Here" append-icon="mdi-magnify" />
+        <v-text-field @click="search($event)" @keyup.enter="search($event)" rounded outlined dense placeholder="Search Here" append-icon="mdi-magnify" />
       </v-form>
     </v-col>
     <v-spacer />
+    <v-btn v-if="this.$store.state.id" icon>
     <v-menu offset-y v-if="this.$store.state.id">
       <template v-slot:activator="{ on, attrs }">
         <span id="bellspan" v-bind="attrs" v-on="on" style="cursor: pointer">
@@ -43,10 +42,9 @@
         </template>
       </v-list>
     </v-menu>
-    <v-btn icon>
-      <v-icon v-if="!this.$store.state.id" @click="$router.push('/login')">mdi-arrow-left-box</v-icon>
     </v-btn>
-    <v-icon v-if="!this.$store.state.id" @click="$router.push('/register')">mdi-account-multiple-plus</v-icon>
+      <v-btn v-if="!this.$store.state.id" @click="$router.push('/login')" elevation="2" style="margin-right:10px;">로그인</v-btn>
+      <v-btn v-if="!this.$store.state.id" @click="$router.push('/register')" elevation="2">회원가입</v-btn>
     <v-btn v-if="this.$store.state.id" icon>
       <v-menu offset-y v-if="this.$store.state.id">
         <template v-slot:activator="{ on, attrs }">
@@ -57,7 +55,7 @@
           </span>
         </template>
         <v-list three-line width="400" height="400">
-          <template v-for="(item, index) in items">
+          <template v-for="(item, index) in messages">
             <div @click="pageMove(item)" style="background-color: white">
               <v-subheader v-if="item.header" :key="item.header" v-text="item.header"></v-subheader>
               <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
@@ -114,15 +112,6 @@ export default {
   unmounted() { },
 
   methods: {
-    test() {
-      console.log(this.items);
-    },
-    logout() {
-      this.$store.commit("setIsLoginFalse");
-      this.$store.commit("logout");
-      this.$store.commit("setUserData", null);
-      this.$router.push("/");
-    },
     //알림정보 가져오기
     getAllNotice() {
       let vm = this;
@@ -140,7 +129,6 @@ export default {
             if (res.data[i].noticeType == 2) {
               vm.messages.unshift({ divider: true, inset: true });
               vm.messages.unshift(res.data[i])
-
             }
             else {
               vm.items.unshift({ divider: true, inset: true });
@@ -153,9 +141,7 @@ export default {
         .catch((err) => {
           console.log(err);
         });
-
     },
-
     //알림 처리
     noticeRes() {
       let vm = this;
@@ -163,7 +149,6 @@ export default {
         function (res) {
           let resNotice = JSON.parse(res.body);
           console.log(resNotice);
-          if (resNotice.memberId != vm.$store.state.id) {
             //sns 알림 처리
             if (resNotice.noticeType == 0) {
               //sns - 좋아요 알림 처리
@@ -185,17 +170,20 @@ export default {
                 noticeType: resNotice.noticeType,
                 noticeId: resNotice.noticeId,
               });
-              vm.items.unshift({ divider: true, inset: true });
-              ++vm.noticeCount;
+              vm.items.unshift({ divider: true, inset: true })
+              ++vm.noticeCount
             }
             //소모임 알림 처리
             else if (resNotice.noticeType == 1) {
               //소모임 댓글 알림 처리
-              vm.items.unshift({ divider: true, inset: true });
+              vm.items.unshift({ divider: true, inset: true })
               if (resNotice.contentType == 0) {
-                vm.subtitle = "댓글을 남기셨습니다.";
+                vm.subtitle = "댓글을 남기셨습니다."
               } else if (resNotice.contentType == 1) {
-                vm.subtitle = "새로운 게시글이 등록되었습니다.";
+                vm.subtitle = "새로운 게시글이 등록되었습니다."
+              }else if(resNotice.contentType == 2){
+                vm.subtitle="님이 언급했어요!"
+                console.log("안녕하세요")
               }
               vm.items.unshift({
                 avatar: require(`@/assets/image/moim/${resNotice.profileImge}`),
@@ -228,11 +216,11 @@ export default {
               }
               ++vm.noticeMsgCount;
             }
-          }
         }
       );
     },
 
+    //알림 클릭 이벤트
     pageMove(item) {
       if (item.noticeType != 2) {
         for (let i = 0; i < this.items.length; i++) {
@@ -273,6 +261,9 @@ export default {
         this.$router.push({ name: "chat", params: { getRoomId: item.postId } });
       }
     },
+    search(event) {
+  
+    }
   },
 };
 </script>
--- mohobby/mohobby/src/components/moim/moimDetail/schedule/MoimSchedule.vue
+++ mohobby/mohobby/src/components/moim/moimDetail/schedule/MoimSchedule.vue
@@ -12,7 +12,7 @@
             text
             small
             color="grey darken-2"
-            @click="prev"
+            @click="prev()"
           >
             <v-icon small>
               mdi-chevron-left
@@ -25,7 +25,7 @@
             text
             small
             color="grey darken-2"
-            @click="next"
+            @click="next()"
           >
             <v-icon small>
               mdi-chevron-right
@@ -33,7 +33,7 @@
           </v-btn>
           
           <v-toolbar-title v-if="calendar">
-            {{ calendar.title }}
+            {{today}}
           </v-toolbar-title>
           <v-spacer></v-spacer>
           <v-menu
@@ -46,7 +46,7 @@
 
         <!-- 달력 부분 -->
         <v-sheet height="500" max-width="800">
-          <v-calendar ref="calendar" :start="start" @click:date="open" :type="type" ></v-calendar>
+          <v-calendar   v-model="value" ref="calendar" :start="start" @click:date="open" :type="type" ></v-calendar>
         </v-sheet>
       </v-col> 
     </v-row>
@@ -63,30 +63,37 @@ import MakeSchedule from "./MakeSchedule.vue"
 export default {
   data() {
     return {
+      month:'',
+      year:'',
+      value: '',
       dialog: false,
+      today:'',
       //받아오는 값 저장할 변수 지정
       calendar: {
         startDate: '',
         startTime: '',
-        hasTime: '',
-        year: '',
-        month: '',
-        title: this.$moment().format('YYYY-MM')
-            },
-      dateOpen: false,
-      start: '2022-10-07',
-      type: 'month',
-      typeOptions: [
-        { text: 'Day', value: 'day' },
-        { text: 'Week', value: 'week' },
-        { text: 'Month', value: 'month' },
-      ],
-      colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-      names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
-    }
+        info: '',
+        endDate: '',
+        endTime: '',
+        title: '',
+        dateOpen: false,
+        start: '2022-10-07',
+        type: 'month',
+        memberId: this.$route.params.moimId,
+      }
+    }},
+
+  created(){
+    this.todate()
   },
   components: { MakeSchedule },
   methods: {
+    todate() {
+      var day = new Date();
+      this.year = day.getFullYear();
+      this.month = ("0" + (day.getMonth() + 1)).slice(-2);
+      this.today=this.year+"-"+this.month;
+    },
     open(date) {
       console.log(date)
       //받아온 데이터 변수에 저장
@@ -103,15 +110,31 @@ export default {
     dialogClose(name) {
       console.log(name);
       this.dialog = false;
-    }
-  },
-  mounted: {
+    },
     prev () {
-      this.$refs.calendar.month.prev()
+      this.month--
+      if(this.month<1){
+        this.month=12;
+        this.year--;
+      }
+      this.today=this.year+"-"+this.month;
+      this.$refs.calendar.prev()
     },
     next () {
-      this.$refs.calendar.month.next()
+      this.month++
+      if(this.month>12){
+        this.month=1;
+        this.year++;
+      }
+      this.today=this.year+"-"+this.month;
+      this.$refs.calendar.next()
     },
+  },
+  mounted: {
+
+  },
+  create(){
+this.calendar.title = new Date()
   }
 }
 </script>
