ddac4b5518c6941f43dc6f8e66e8d653163d3290
gabin <rlarkqlskb@naver.com>
2022-11-01 12:50:49 +0900

    ssss

--- mohobby/mohobby/src/views/main/MainView.vue
+++ mohobby/mohobby/src/views/main/MainView.vue
@@ -9,50 +9,35 @@
         ></v-carousel-item>
       </v-carousel>
     </div>
-    <v-img
-      :src="require('@/assets/image/web/back-1.jpg')"
-      max-height="900"
-    >
+    <v-img :src="require('@/assets/image/web/back-1.jpg')" max-height="900">
       <v-container fluid class="mt-10">
         <div class="d-flex justify-center">
           <v-chip :ripple="false" class="pa-4" large dark color="#2255b1">
-            <h1 style="font-weight: bolder;">Best Feed in SNS</h1>
+            <h1 style="font-weight: bolder">Best Feed in SNS</h1>
           </v-chip>
         </div>
         <MainSnsList :snsHighLikesList="snsHighLikesList"></MainSnsList>
       </v-container>
     </v-img>
-    <v-img
-      :src="require('@/assets/image/web/back-2.jpg')"
-    />
-    <v-img
-      :src="require('@/assets/image/web/back-3.jpg')"
-      max-height="700"
-    >
+    <v-img :src="require('@/assets/image/web/back-2.jpg')" />
+    <v-img :src="require('@/assets/image/web/back-3.jpg')" max-height="700">
       <div class="d-flex justify-center pb-10">
         <v-chip :ripple="false" class="pa-4" large dark color="#2255b1">
-          <h1 style="font-weight: bolder;">Best CLUB</h1>
+          <h1 style="font-weight: bolder">Best CLUB</h1>
         </v-chip>
       </div>
       <moimPopularList :moimPopularList="moimPopularList"></moimPopularList>
     </v-img>
-    <v-img
-      :src="require('@/assets/image/web/back-4.jpg')"
-    />
-    <v-img
-      :src="require('@/assets/image/web/back-5.jpg')"
-      max-height="700"
-    >
+    <v-img :src="require('@/assets/image/web/back-4.jpg')" />
+    <v-img :src="require('@/assets/image/web/back-5.jpg')" max-height="700">
       <div class="d-flex justify-center">
         <v-chip :ripple="false" class="pa-4" large dark color="#2255b1">
-          <h1 style="font-weight: bolder;">Take a CLASS</h1>
+          <h1 style="font-weight: bolder">Take a CLASS</h1>
         </v-chip>
       </div>
       <collectClassList :collectClassList="collectClassList"></collectClassList>
     </v-img>
-    <v-img
-      :src="require('@/assets/image/web/footer.jpg')"
-    />
+    <v-img :src="require('@/assets/image/web/footer.jpg')" />
   </div>
 </template>
 
@@ -67,7 +52,7 @@ export default {
     moimPopularList,
     snsHighLikesList,
     collectClassList,
-    MainSnsList
+    MainSnsList,
   },
 
   data() {
@@ -96,7 +81,7 @@ export default {
   mounted() {
     this.getSnsList();
     this.getMoimList();
-    this.getClassList();
+    // this.getClassList();
   },
   unmounted() {},
   methods: {
@@ -121,7 +106,7 @@ export default {
         method: "get",
       })
         .then(function (response) {
-          for(let i = 0; i < response.data.length; i++) {
+          for (let i = 0; i < response.data.length; i++) {
             response.data[i].show = false;
           }
           vm.moimPopularList = response;
@@ -131,19 +116,19 @@ export default {
         });
     },
 
-    getClassList() {
-      const vm = this;
-      this.axios({
-        url: "/mainclasslist",
-        method: "get",
-      })
-        .then(function (response) {
-          vm.collectClassList = response;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    },
+    // getClassList() {
+    //   const vm = this;
+    //   this.axios({
+    //     url: "/mainclasslist",
+    //     method: "get",
+    //   })
+    //     .then(function (response) {
+    //       vm.collectClassList = response;
+    //     })
+    //     .catch(function (error) {
+    //       console.log(error);
+    //     });
+    // },
   },
 };
 </script>
