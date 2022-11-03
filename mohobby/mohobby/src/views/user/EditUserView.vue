19d4f5a03d1f3edb41be0783991770e00115606b
gabin <rlarkqlskb@naver.com>
2022-11-03 12:27:41 +0900

    2022-11-03 은경, 휘동 수정

--- mohobby/mohobby/src/views/user/EditUserView.vue
+++ mohobby/mohobby/src/views/user/EditUserView.vue
@@ -314,11 +314,6 @@ export default {
     setMemberInfo() {
       const vm = this;
       this.memberId = this.$store.state.id;
-      console.log(this.memberId);
-      console.log(this.memberId);
-      console.log(this.memberId);
-      console.log(this.memberId);
-      console.log(this.memberId);
       this.axios({
         url: "/member/" + this.memberId,
         method: "get",
@@ -346,15 +341,15 @@ export default {
     },
     // 날짜계산
     calDate(milliSecond) {
-        const days = ['일', '월', '화', '수', '목', '금', '토'];
-        const data = new Date(milliSecond);  //Date객체 생성
+      const days = ["일", "월", "화", "수", "목", "금", "토"];
+      const data = new Date(milliSecond); //Date객체 생성
 
-        const year = data.getFullYear();    //0000년 가져오기
-        const month = data.getMonth() + 1;  //월은 0부터 시작하니 +1하기
-        const date = data.getDate();        //일자 가져오기
-        const day = days[data.getDay()];    //요일 가져오기
+      const year = data.getFullYear(); //0000년 가져오기
+      const month = data.getMonth() + 1; //월은 0부터 시작하니 +1하기
+      const date = data.getDate(); //일자 가져오기
+      const day = days[data.getDay()]; //요일 가져오기
 
-        return `${year}-${month}-${date}`;
+      return `${year}-${month}-${date}`;
     },
     // 관심사 전체 목록 가져오기
     getAllCatg() {
