abf7ecb2fda00f5921d317a6a83a5ca2f6fdb2b2
gabin <rlarkqlskb@naver.com>
2022-11-03 16:47:38 +0900

    2022-11-03 선진 수정

--- mohobby/mohobby/src/components/sns/Management/User.vue
+++ mohobby/mohobby/src/components/sns/Management/User.vue
@@ -1,59 +1,80 @@
 <template>
   <div>
     <v-card class="mx-auto" width="800" min-height="520">
-      <v-toolbar color="#2255b1" dark> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ keyword }}</v-toolbar>
+      <v-toolbar color="#2255b1" dark>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ keyword }}</v-toolbar
+      >
       <v-list three-line>
         <div v-for="(user, i) in userResult" :key="i">
           <template>
-       
             <v-list-item>
               <v-list-item-avatar>
                 <!-- 팔로워 목록 클릭시 -->
-                <v-img v-if="followType === 1" @click="goUserProfile(user.followingId)"
-                  :src="require(`@/assets/image/user/${user.profileImg}`)" />
+                <v-img
+                  v-if="followType === 1"
+                  @click="goUserProfile(user.followingId)"
+                  :src="require(`@/assets/image/user/${user.profileImg}`)"
+                />
 
                 <!-- 팔로잉 목록 클릭시 -->
-                <v-img v-else-if="followType === 2" @click="goUserProfile(user.followerId)"
-                  :src="require(`@/assets/image/user/${user.profileImg}`)" />
+                <v-img
+                  v-else-if="followType === 2"
+                  @click="goUserProfile(user.followerId)"
+                  :src="require(`@/assets/image/user/${user.profileImg}`)"
+                />
 
                 <!-- 유저검색 -->
-                <v-img v-else-if="followType === 3" @click="goUserProfile(user.memberId)" :src="require(`@/assets/image/user/${user.profileImg}`)" />
+                <v-img
+                  v-else-if="followType === 3"
+                  @click="goUserProfile(user.memberId)"
+                  :src="require(`@/assets/image/user/${user.profileImg}`)"
+                />
               </v-list-item-avatar>
               <v-list-item-content>
-
-
                 <div v-if="followType === 1">
                   <!-- 팔로워 목록 : followerId -> 구독 당하는 사람이 나임  -->
                   <v-list-item-title v-html="user.followingId" />
                 </div>
 
-                
                 <!-- 팔로잉 목록 : followingId -> 구독 하는 사람이 나임 -->
                 <div v-else-if="followType === 2">
                   <v-list-item-title v-html="user.followerId" />
                 </div>
 
-
                 <!-- 유저검색 목록: memberId -->
                 <div v-else>
-                  <v-list-item-title v-show="user.memberId" v-html="user.memberId" />
+                  <v-list-item-title
+                    v-show="user.memberId"
+                    v-html="user.memberId"
+                  />
                   <v-list-item-subtitle v-html="user.nickname" />
                 </div>
               </v-list-item-content>
 
-
               <!-- 팔로우 = 내가 구독 안한상태면 활성화 -->
               <div v-if="followType == 1">
-                <v-btn v-if="followerCheck[i] == 1" @click="unfollow(user.followerId,user.followingId)" rounded
-                  color="#F36A3E" class="white--text">언팔로우</v-btn>
+                <v-btn
+                  v-if="followerCheck[i] == 1"
+                  @click="unfollow(user.followerId, user.followingId)"
+                  rounded
+                  color="#F36A3E"
+                  class="white--text"
+                  >언팔로우</v-btn
+                >
 
-                <v-btn v-else-if="followerCheck[i] == 0" @click="followup(user.followingId, user.followerId)" rounded
-                  color="#2255b1" class="white--text">팔로우</v-btn>
+                <v-btn
+                  v-else-if="followerCheck[i] == 0"
+                  @click="followup(user.followingId, user.followerId)"
+                  rounded
+                  color="#2255b1"
+                  class="white--text"
+                  >팔로우</v-btn
+                >
               </div>
-              <div v-else>
+              <!-- <div v-else>
                 <v-btn @click="unfollow(user.followerId,user.followingId)" rounded color="#2ac187" class="white--text"> 언팔로우
                 </v-btn>
-              </div>
+              </div> -->
             </v-list-item>
           </template>
         </div>
@@ -74,10 +95,8 @@ export default {
   },
   data() {
     return {
-
       users: [], //props 담을 변수
       memberId: this.$store.state.id,
-
     };
   },
   created() {
@@ -95,7 +114,6 @@ export default {
   //   }
   // },
 
-
   methods: {
     //유저 프로필로 이동
     goUserProfile(userId) {
@@ -104,8 +122,7 @@ export default {
 
     //팔로우
     followup(memberId, userId) {
-      this.follower = [],
-        console.log("memberId : " + memberId);
+      (this.follower = []), console.log("memberId : " + memberId);
       console.log("userId : " + userId);
 
       this.axios
@@ -115,29 +132,27 @@ export default {
         })
         .then((res) => {
           // this.followStatus = 1;
-          this.$emit("updateFollow")
+          this.$emit("updateFollow");
         })
         .catch((err) => {
           console.log(err);
         });
-
     },
 
-
     //언팔로우
-    unfollow(memberId, userId) { //내 아이디, userId
+    unfollow(memberId, userId) {
+      //내 아이디, userId
       this.axios
         .delete("/sns/follow/" + memberId + "/" + userId)
         .then((res) => {
-      
           console.log("언팔로우 성공! " + res);
-     
-      this.$emit("updateFollow")
+
+          this.$emit("updateFollow");
         })
         .catch((err) => {
           console.log(err);
         });
     },
-  }
-}
+  },
+};
 </script>
